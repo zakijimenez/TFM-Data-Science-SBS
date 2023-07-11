fa1fc4cceaa8e5e1c26d7ab64b6a4f3d55ae6e1c
cigverse <139259665+cigverse@users.noreply.github.com>
2023-07-11 19:37:08 +0200

    Carga Datasets y Analisis Exploratorio en python

---
+++ exploratory.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train_data = pd.read_csv('train.csv')
+
+train_data.head()
+#train_data.shape()
+train_data.info()
